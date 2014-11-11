1aa282ab5f3eba28db2787e64f5a2601c3319209
John Rickwood <github@rickwood.co.uk>
2014-11-11 20:22:27 +0000

    now uses parameters provided on the command line for the image folders rather than being hard coded in the file

--- duplicates.rb
+++ duplicates.rb
@@ -4,33 +4,39 @@ require 'digest/md5'
 require 'exifr'
 require 'fileutils'
 require 'logger'
-
-imagesDir = "images"
-imagesToCheck = "check"
-duplicates = "duplicates"
+require 'optparse'
 
 currentDateTime = Time.now.strftime("%Y%m%d-%H%M")
-
 
 logger = Logger.new 'output_' + currentDateTime + '.log'
 images = []
 
+options = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: duplicates.rb [options]"
+    
+    opts.on('-i', '--images FOLDER', 'Images folder - main folder of images') { |v| options[:images] = v }
+    opts.on('-c', '--check FOLDER', 'Images to check - program will check if any images in this folder exists in the main image folder') { |v| options[:check] = v }
+    opts.on('-d', '--duplicates FOLDER', 'Duplicates folder - duplicate images are moved to this destination') { |v| options[:duplicates] = v }
+    
+end.parse!
+
 # load contents of imagesDir into an array 
-Dir.glob(imagesDir + "/**/*.jpg") do |my_image_file|
+Dir.glob(options[:images] + "/**/*.jpg") do |my_image_file|
   logger.info "adding #{my_image_file} to the array"
   hash = Digest::MD5.file my_image_file
   images << hash
 end
 
 # loop through images in imagesToCheck and check if they already exist
-Dir.glob(imagesToCheck + "/**/*.jpg") do |my_image_file|
+Dir.glob(options[:check] + "/**/*.jpg") do |my_image_file|
   logger.info "checking if #{my_image_file} already exists"
   hash = Digest::MD5.file my_image_file
   result = images.include? hash
   if result
-    logger.info "image is a duplicate - moving it to " + duplicates
+    logger.info "image is a duplicate - moving it to " + options[:duplicates]
     filename = File.basename(my_image_file)
-    newLocation = File.join(duplicates,filename)
+    newLocation = File.join(options[:duplicates],filename)
     FileUtils.mv(my_image_file, newLocation)
   else
     logger.info "image is not a duplicate - leaving it where it is "
